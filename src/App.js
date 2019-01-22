0ae92a05293f160a5228d8108ae6f12ae02cdc22
Sergei <staroduber@yandex.ru>
2019-01-22 20:49:25 +0400

    1

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import './app.scss';
+
+class App extends Component {
+  render() {
+    return (
+      <div id = 'gg'>
+        руддщ
+      </div>
+    );
+  }
+}
+
+export default App;
