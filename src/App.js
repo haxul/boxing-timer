b0543cadd5f9da6608ef08896dff7b175123c681
Sergei <staroduber@yandex.ru>
2019-01-23 20:24:45 +0400

    hello

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React, { Component } from 'react';
 import './app.scss';
 import Clock from './components/Clock';
+import Sidebar from './components/Sidebar';
 
 class App extends Component {
   render() {
     return (
       <div className = 'container'>
       <Clock/>
+      <Sidebar/>
       </div>
     );
   }
