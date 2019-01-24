b4b77f7375b920e93062ec86fe39b385646d2ac5
Sergei <staroduber@yandex.ru>
2019-01-24 20:58:56 +0400

    1

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -2,9 +2,14 @@ import React from 'react';
 
 class Sidebar extends React.Component {
   render () {
-    return (
-      <div className='container'>
 
+    return (
+      <div className='container' id = 'sidebar'>
+        <div className = 'blocks'>Rounds <input type="text" id = 'input1' defaultValue = '3'/></div>
+        <div className = 'blocks'>Round Time <input type="text" id = 'input4' defaultValue = '03:00'/></div>
+        <div className = 'blocks'>Rest Time <input type="text" id = 'input5'defaultValue = '01:00'/></div>
+        <div className = 'blocks'>Breaking <input type="text" id = 'input2' defaultValue = '00:30'/></div>
+        <div className = 'blocks'>Prepare <input type="text" id = 'input3'defaultValue = '00:25'/></div>
       </div>
     )
   }
