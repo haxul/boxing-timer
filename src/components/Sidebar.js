f1e86810d09451b966c005f743fb9270edc40c48
Sergei <staroduber@yandex.ru>
2019-01-25 22:36:23 +0400

    timer

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -7,10 +7,10 @@ class Sidebar extends React.Component {
   //
   state = {
     round: 3,
-    roundTime: '03:00',
-    restTime: '01:00',
-    prepareTime: '00:30',
-    alerter: '00:30'
+    roundTime: "03:00",
+    restTime: "01:00",
+    prepareTime: "00:30",
+    alerter: "00:30"
   };
 
   handleRound = e => {
@@ -27,47 +27,45 @@ class Sidebar extends React.Component {
   };
 
   setTime = (e, item, maxMin, minMin, maxSec, minSec) => {
-    const addZero = (n) => {
+    const addZero = n => {
       if (+n > 9) return n;
-      return ('0' + n);
-    }
-    let minutes = parseInt(item.slice(0,2)),
-        secundes = parseInt(item.slice(-2));
-    if (e.key === 'ArrowUp') {
+      return "0" + n;
+    };
+    let minutes = parseInt(item.slice(0, 2)),
+      secundes = parseInt(item.slice(-2));
+    if (e.key === "ArrowUp") {
       if (secundes > 40) {
         secundes = 0;
         minutes++;
-      } else if (minutes >= maxMin && secundes >= maxSec) { //maximum
+      } else if (minutes >= maxMin && secundes >= maxSec) {
+        // define the maximum
         minutes = maxMin;
         secundes = maxSec;
-      }
-      else secundes+=10;
-    } else if (e.key === 'ArrowDown') {
-      if (minutes === minMin && secundes === minSec) { //minimum
+      } else secundes += 10;
+    } else if (e.key === "ArrowDown") {
+      if (minutes === minMin && secundes === minSec) {
+        //define the minimum
         minutes = minMin;
         secundes = minSec;
-      }
-      else if (secundes <= 0 ) {
-        secundes = 50
+      } else if (secundes <= 0) {
+        secundes = 50;
         minutes--;
-      }
-      else secundes-=10;
+      } else secundes -= 10;
     }
-    item = addZero(minutes) + ':' + addZero(secundes);
+    item = addZero(minutes) + ":" + addZero(secundes);
     return item;
-  }
-
+  };
 
   handleInputs = e => {
-    this.setState({round: this.state.round });
-    this.setState({roundTime: this.state.roundTime});
-    this.setState({restTime: this.state.restTime});
-    this.setState({prepareTime: this.state.prepareTime});
-    this.setState({alerter: this.state.alerter});
+    this.setState({ round: this.state.round });
+    this.setState({ roundTime: this.state.roundTime });
+    this.setState({ restTime: this.state.restTime });
+    this.setState({ prepareTime: this.state.prepareTime });
+    this.setState({ alerter: this.state.alerter });
   };
 
   render() {
-    const { round, roundTime, restTime, prepareTime, alerter} = this.state;
+    const { round, roundTime, restTime, prepareTime, alerter } = this.state;
     return (
       <div className="container" id="sidebar">
         <div className="blocks">
@@ -81,28 +79,56 @@ class Sidebar extends React.Component {
           />
         </div>
         <div className="blocks">
-          Round Time <input type="text" id="input4" value = {roundTime} onChange = {this.handleInputs} onKeyDown = {(e) => {
-            let value = this.setTime(e,roundTime, 10,0,0,10);
-            this.setState({roundTime: value});
-          }}  />
+          Round Time{" "}
+          <input
+            type="text"
+            id="input4"
+            value={roundTime}
+            onChange={this.handleInputs}
+            onKeyDown={e => {
+              let value = this.setTime(e, roundTime, 10, 0, 0, 10);
+              this.setState({ roundTime: value });
+            }}
+          />
         </div>
         <div className="blocks">
-          Rest Time <input type="text" id="input5" value={restTime} onChange = {this.handleInputs} onKeyDown = {(e) => {
-            let value = this.setTime(e,restTime, 10,0,0,10);
-            this.setState({restTime: value});
-          }}/>
+          Rest Time{" "}
+          <input
+            type="text"
+            id="input5"
+            value={restTime}
+            onChange={this.handleInputs}
+            onKeyDown={e => {
+              let value = this.setTime(e, restTime, 10, 0, 0, 10);
+              this.setState({ restTime: value });
+            }}
+          />
         </div>
         <div className="blocks">
-          Alerter <input type="text" id="input2" value={alerter} onChange = {this.handleInputs} onKeyDown = {(e) => {
-            let value = this.setTime(e,alerter,1,0,0,0);
-            this.setState({alerter: value});
-          }}/>
+          Alerter{" "}
+          <input
+            type="text"
+            id="input2"
+            value={alerter}
+            onChange={this.handleInputs}
+            onKeyDown={e => {
+              let value = this.setTime(e, alerter, 1, 0, 0, 0);
+              this.setState({ alerter: value });
+            }}
+          />
         </div>
         <div className="blocks">
-          Prepare <input type="text" id="input3" value= {prepareTime} onChange = {this.handleInputs} onKeyDown = {(e) => {
-            let value = this.setTime(e, prepareTime, 1,0,0,10);
-            this.setState({prepareTime: value})
-          }}/>
+          Prepare{" "}
+          <input
+            type="text"
+            id="input3"
+            value={prepareTime}
+            onChange={this.handleInputs}
+            onKeyDown={e => {
+              let value = this.setTime(e, prepareTime, 1, 0, 0, 10);
+              this.setState({ prepareTime: value });
+            }}
+          />
         </div>
       </div>
     );
