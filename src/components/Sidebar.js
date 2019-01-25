bbaf1e7ba216e37e656089bc51ec371d86dd9f2f
Sergei <staroduber@yandex.ru>
2019-01-25 22:32:06 +0400

    timer

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,18 +1,112 @@
-import React from 'react';
+import React from "react";
 
 class Sidebar extends React.Component {
-  render () {
+  // constructor () {
+  //   super();
+  // }
+  //
+  state = {
+    round: 3,
+    roundTime: '03:00',
+    restTime: '01:00',
+    prepareTime: '00:30',
+    alerter: '00:30'
+  };
 
+  handleRound = e => {
+    let { round } = this.state;
+    round = Number(round);
+    if (e.key === "ArrowUp") {
+      if (round > 14) round = 14;
+      round++;
+    } else if (e.key === "ArrowDown") {
+      if (round < 2) round = 2;
+      round--;
+    }
+    this.setState({ round: round });
+  };
+
+  setTime = (e, item, maxMin, minMin, maxSec, minSec) => {
+    const addZero = (n) => {
+      if (+n > 9) return n;
+      return ('0' + n);
+    }
+    let minutes = parseInt(item.slice(0,2)),
+        secundes = parseInt(item.slice(-2));
+    if (e.key === 'ArrowUp') {
+      if (secundes > 40) {
+        secundes = 0;
+        minutes++;
+      } else if (minutes >= maxMin && secundes >= maxSec) { //maximum
+        minutes = maxMin;
+        secundes = maxSec;
+      }
+      else secundes+=10;
+    } else if (e.key === 'ArrowDown') {
+      if (minutes === minMin && secundes === minSec) { //minimum
+        minutes = minMin;
+        secundes = minSec;
+      }
+      else if (secundes <= 0 ) {
+        secundes = 50
+        minutes--;
+      }
+      else secundes-=10;
+    }
+    item = addZero(minutes) + ':' + addZero(secundes);
+    return item;
+  }
+
+
+  handleInputs = e => {
+    this.setState({round: this.state.round });
+    this.setState({roundTime: this.state.roundTime});
+    this.setState({restTime: this.state.restTime});
+    this.setState({prepareTime: this.state.prepareTime});
+    this.setState({alerter: this.state.alerter});
+  };
+
+  render() {
+    const { round, roundTime, restTime, prepareTime, alerter} = this.state;
     return (
-      <div className='container' id = 'sidebar'>
-        <div className = 'blocks'>Rounds <input type="text" id = 'input1' defaultValue = '3'/></div>
-        <div className = 'blocks'>Round Time <input type="text" id = 'input4' defaultValue = '03:00'/></div>
-        <div className = 'blocks'>Rest Time <input type="text" id = 'input5'defaultValue = '01:00'/></div>
-        <div className = 'blocks'>Breaking <input type="text" id = 'input2' defaultValue = '00:30'/></div>
-        <div className = 'blocks'>Prepare <input type="text" id = 'input3'defaultValue = '00:25'/></div>
+      <div className="container" id="sidebar">
+        <div className="blocks">
+          Rounds{" "}
+          <input
+            type="text"
+            id="input1"
+            value={round}
+            onKeyDown={this.handleRound}
+            onChange={this.handleInputs}
+          />
+        </div>
+        <div className="blocks">
+          Round Time <input type="text" id="input4" value = {roundTime} onChange = {this.handleInputs} onKeyDown = {(e) => {
+            let value = this.setTime(e,roundTime, 10,0,0,10);
+            this.setState({roundTime: value});
+          }}  />
+        </div>
+        <div className="blocks">
+          Rest Time <input type="text" id="input5" value={restTime} onChange = {this.handleInputs} onKeyDown = {(e) => {
+            let value = this.setTime(e,restTime, 10,0,0,10);
+            this.setState({restTime: value});
+          }}/>
+        </div>
+        <div className="blocks">
+          Alerter <input type="text" id="input2" value={alerter} onChange = {this.handleInputs} onKeyDown = {(e) => {
+            let value = this.setTime(e,alerter,1,0,0,0);
+            this.setState({alerter: value});
+          }}/>
+        </div>
+        <div className="blocks">
+          Prepare <input type="text" id="input3" value= {prepareTime} onChange = {this.handleInputs} onKeyDown = {(e) => {
+            let value = this.setTime(e, prepareTime, 1,0,0,10);
+            this.setState({prepareTime: value})
+          }}/>
+        </div>
       </div>
-    )
+    );
   }
 }
 
-export default Sidebar
+export default Sidebar;
